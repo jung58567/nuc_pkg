5950916f7ee083fc4690239e8682f763e9b45cfa
jungyong lee <jung58567@naver.com>
2023-03-16 18:53:52 +0900

    second commit

---
+++ kubot/src/main_recent.cpp
@@ -0,0 +1,531 @@
+#include "kubot.h"
+#include "CKubot.h"
+#include "kinematics.h"
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#include "e2box_imu_9dofv4.h"
+#include "main.h"
+#include "t_serial.h"
+
+class e2box_imu_9dofv4 m_e2box_imu;
+class IMU imu;
+using namespace std;
+
+DXL dxl("/dev/ttyDXL",2000000);
+dxl_controller DXL_Controller_;
+Kinematics KM;
+CKubot C;
+
+#define onesizeSec 100
+#define NSEC_PER_SEC 1000000000     // 10^9
+#define EC_TIMEOUTMON 500
+
+enum EndPointAxis {
+    X = 0, Y, Z, Roll, Pitch, Yaw
+};
+
+typedef struct {
+    VectorXd BA, LF, RF;
+} GLOBAL;
+
+GLOBAL Global;
+
+typedef struct {
+    VectorXd init;
+    VectorXd cal;
+    VectorXd fin;
+    VectorXd pre;
+} JOINT;
+
+typedef struct {
+    JOINT R;
+    JOINT L;
+} JointVariables;
+
+JointVariables q;
+
+VectorXi jointdirection_L(6), jointdirection_R(6);
+// double SL, RL, FH; 
+
+unsigned int cycle_ns = 1000000;    // 10^6ns = 1ms
+
+double present_pos[12];
+int converted_pos[12];
+
+int cnt = 0;
+double t = 0.0;
+double T = 1;
+double thread_t=0.0;
+
+double initialize_ready_time=2.0;
+double tt = 0.0;
+int cnt3=0;
+
+int cnt2 =0;
+double thread_t2[6][1000]={0,};//0==cnt, 1==Euler, 2==Ang_vel, 3==thread_t 4==
+
+int flag_s = 0;
+
+bool thread_is_running = true;
+bool initialize2 =true;
+
+//***************Xenomai time variables***************//
+
+RT_TASK RT_task1,RT_task2;
+RTIME now, previous;    // System Test time
+RTIME now1, previous1;
+RTIME now2, previous2;  // Thread 1 cycle time
+
+double t_s,t_imu_s,t_pub_s,t_imu_d,t_pub_d,t_l,t_s_r;
+
+void initializeKubot();
+void save_file();
+void serial_task(void* arg);
+void catch_signal(int sig);
+void dxl_switch_Callback(const std_msgs::String::ConstPtr& msg) {
+     ROS_INFO("data: [%s]", msg->data.c_str());
+     if(msg->data == "tq_off") {
+        for(int i=1; i<20; i++) {
+            //dxl.Torque_Off(i);
+        }
+     }
+     else if(msg->data == "Ready") {
+        while(1) {
+            // if (t <= C.zmp.previewControl.PreviewReadyTime) {
+            //     // init_pose();
+                break;
+            // }
+            // now1 = rt_timer_read();
+            //  thread_t += (long)(now1-previous1)/cycle_ns;
+            // // cout << "Thread_time : " << int(t/cycle_ns) << "s  dt : " << (long)(now1-previous1)/1000 << "ns" << endl;
+            // previous1 = now1;
+        }
+    }
+    return;
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "kubot_node");
+    ros::NodeHandle nh;
+
+//imu때문에 추가한 코드
+    std::string port;
+    int baudrate;
+
+    nh.param<std::string>("port", port, "/dev/ttyUSB1");
+    nh.param("baudrate", baudrate, 115200);
+
+        if(!m_e2box_imu.serial.Open(const_cast<char*>(port.c_str()), baudrate)){
+        cout << "device is not opened! " << endl;
+    }
+//imu
+
+
+    ROS_INFO("run main.cpp");
+    ros::Subscriber sub = nh.subscribe("dxl_switch", 1000, dxl_switch_Callback);
+    // ros::Publisher chatter_pub = nh.advertise<std_msgs::String>("chatter", 1000);
+    ros::Rate loop_rate(10);
+
+    signal(SIGINT, catch_signal);
+    signal(SIGTERM,catch_signal);
+    
+    Load();
+    
+    //(PKW)
+    rt_task_create(&RT_task1, "serial_task", 0, 99, 0);
+    rt_task_start(&RT_task1, &serial_task, NULL);
+    
+    cout << "while 전 " << endl;
+
+    while (ros::ok())
+    {
+        // std_msgs::String msg;
+        // msg.data = "I said hello";
+        
+        // chatter_pub.publish(msg);
+        // cout << "while 전 " << endl;
+        ros::spinOnce();
+        loop_rate.sleep();
+    
+    }
+    
+    cout << "while 끝 thread is running =false 이전 " << endl;    
+    // initializeKubot();
+    //while까지만 실행되는 듯 밑에 있는 thread_is_running = false은 실행되지 않음
+    thread_is_running = false;
+    
+    return 0;
+}
+
+
+
+void save_file(){
+    string line;
+    ofstream file;
+    file.open("time.txt",ios_base::app);
+    if(file.is_open()) {
+		file <<"count"<<","<<"Euler"<<","<<"ang_vel"<<","<<"dt_imu"<<","<<"process_thread"<<","<<"dt_thread"<<"\n";
+        for(int k=1;k<=cnt2;k++)
+        {
+            for(int u=1;u<=6;u++){  
+            file <<thread_t2[u-1][k-1]<<",";  
+            }
+        file<<"\n";
+        }
+        cout<<"save done"<<endl;
+		file.close();
+	}
+    else {
+		cout << "error" << endl;	
+	}
+}
+
+
+
+void initializeKubot() {
+
+    Global.BA = VectorXd::Zero(6);
+    Global.LF = VectorXd::Zero(6);
+    Global.RF = VectorXd::Zero(6);
+
+    Global.BA(Z) = 0.308;// 0.308;
+    Global.LF(Y) = 0.05;
+    Global.RF(Y) = -0.05;
+
+    q.R.init = VectorXd::Zero(6);//모든 다리 관절을 0도로 초기화 해준다.
+    q.L.init = VectorXd::Zero(6);
+
+    q.R.cal = KM.Geometric_IK_R(Global.BA, Global.RF);
+    q.L.cal = KM.Geometric_IK_L(Global.BA, Global.LF);
+
+}
+
+void Load()
+{   
+    int encoder_read[12];
+    DXL_Controller_.Initialize();
+    DXL_Controller_.Read_Dxl_Encoder_Once(encoder_read);//처음에 모터값을 읽어줌
+
+    for(int i = 0; i < 12; i++) { // 12번 반복
+    //    ROS_INFO("dxl.present_position[%d]=%d", i+1, dxl.present_position[i]);
+        present_pos[i] = convertw4095ToRad(encoder_read[i]);//처음 읽어준 모터값을 라디안 값으로 바꿔서 출력함
+     ROS_INFO("present_pos[%d]=%f", i+1, present_pos[i]);
+    }
+
+    jointdirection_R << -1,-1,-1,-1, 1, 1;
+    jointdirection_L << -1,-1, 1, 1,-1, 1;
+
+    initializeKubot();//쿠봇 다리 펴게 해주는 함수
+
+    C.initializeCKubot();
+    C.setWalkingStep(10, C.walking.step.S_R_F, 0.03, 0, 0, 0.04);
+    C.setWalkingTime(0.6, 0.4);
+    C.setZmpPreview(1.5);
+    C.initializeZmpTraj();
+    C.FootPlanner();
+    C.generateZmpTraj(C.walking.step.Lfoot, C.walking.step.Rfoot); 
+}
+
+void serial_task(void* arg)
+{
+    // rt_task_set_periodic(NULL, TM_NOW, cycle_ns * 10);
+    rt_task_set_periodic(NULL, TM_NOW, cycle_ns * 5);
+
+    int j=0;
+    now1 = rt_timer_read();
+    previous1 = now1;
+    
+    t_s_r = now1;
+    //t_l=now1;
+
+    while (thread_is_running)
+    {
+        rt_task_wait_period(NULL);
+
+
+        //imu 데이터 받기 위해 추가로 작성한 코드
+        
+        now1 = rt_timer_read();
+        t_s = rt_timer_read();
+        thread_t = (long)(now1-previous1)/1000000.0;
+        previous1 = now1;
+    
+        t_imu_s= rt_timer_read();
+        imu.OnReceiveImu();
+        t_imu_d=rt_timer_read();
+        
+        std::cout<<"imu= "<<m_e2box_imu.Euler[0]<<std::endl;
+        /////////////////////////////////
+
+
+        if(t <= C.zmp.previewControl.PreviewReadyTime) {
+            init_pose();
+        }
+        else if(t < C.zmp.previewControl.RefTotalTime - 4*C.zmp.previewControl.PreviewTime) {
+             process();
+             cout << "t =  "<< t << endl;
+        }
+	    else if(t < C.zmp.previewControl.RefTotalTime){
+              cout<<"process done"<<endl;
+              cout << "t =  "<< t << endl;
+
+              q.L.pre = KM.Geometric_IK_L(Global.BA, Global.LF);
+              q.R.pre = KM.Geometric_IK_R(Global.BA, Global.RF);
+          }
+         //kick_process();
+        // now1 = rt_timer_read();
+        // time += 0.01; // (double)(now1-previous1)/cycle_ns/1000;
+        // cout << "Thread_time : " << t << "s  dt : " << (double)(now1-previous1)/1000 << "ns" << endl;
+        
+
+
+
+        for(int i = 0; i < 6; i++) {
+            converted_pos[2*i]   = convertRadTow4095(jointdirection_R(i)*q.R.pre(i));
+        }
+        for(int i = 0; i < 6; i++) {
+            converted_pos[2*i+1] = convertRadTow4095(jointdirection_L(i)*q.L.pre(i));
+        }
+
+        t = (double)cnt / (double)onesecSize;//단위가 s이다(그래서 100으로 나눠줬네)
+        C.walking.time.sec = t - C.zmp.previewControl.PreviewReadyTime - C.walking.step.current * C.walking.time.periodTime;//sec은 현재시간- 2초 - 현재 발걸음*주기
+        //C.generateFootTraj();-------------> 이건 예전버전이라 주석
+        C.generateFootTraj_ver2();
+        C.zmp.previewControl.count = cnt;
+        C.zmpPreviewControl();
+
+       //if (t < C.zmp.previewControl.RefTotalTime - 2*C.zmp.previewControl.PreviewTime)
+        if (t < C.zmp.previewControl.RefTotalTime - 4*C.zmp.previewControl.PreviewTime) {
+            if(C.walking.time.sec >= C.walking.time.SSP_start_time && C.walking.time.sec <= C.walking.time.SSP_end_time) {
+                if((C.walking.step.current % 2) == 0) {
+                    converted_pos[2] = converted_pos[2] + C.compensate(C.walking.time.sec - C.walking.time.SSP_start_time, C.walking.time.SSP_time, 0.9, 0, 100);
+                    //converted_pos[3] = converted_pos[3] - C.trapezoidal(C.walking.time.sec - C.walking.time.SSP_start_time, C.walking.time.SSP_time, 0.4, 0, 16);
+                }
+                else if((C.walking.step.current % 2) == 1) {
+                    converted_pos[3] = converted_pos[3] - C.compensate(C.walking.time.sec - C.walking.time.SSP_start_time, C.walking.time.SSP_time, 0.9, 0, 100);
+                    //converted_pos[2] = converted_pos[2] + C.trapezoidal(C.walking.time.sec - C.walking.time.SSP_start_time, C.walking.time.SSP_time, 0.4, 0, 16);
+                }
+            }
+        }
+
+//        if (t > 3*T and 7*T >= t) {
+//            converted_pos[3] = converted_pos[3] - C.compensate( t-3*T , 4*T, 0.8, 0, 190);
+//        }
+
+        if(C.walking.time.sec >= C.walking.time.periodTime) {
+            C.walking.time.sec = 0;
+            C.walking.step.current++;
+        }
+
+        DXL_Controller_.Sync_Position_command_TxOnly(converted_pos);
+
+    cnt++;
+   
+        t_l = rt_timer_read();
+
+        thread_t2[0][cnt2]=cnt2;
+        thread_t2[1][cnt2]=m_e2box_imu.Euler[0];
+        thread_t2[2][cnt2]=m_e2box_imu.m_dAngRate[0];
+        thread_t2[3][cnt2]=(long)(t_imu_d - t_imu_s)/1000000.0; // dt_imu_recive
+        thread_t2[4][cnt2]=(long)(t_l - t_s)/1000000.0; //dt_task_thread_time
+        thread_t2[5][cnt2]=(thread_t); //dt_thread
+        //thread_t2[5][cnt2]=(long)(t_pub_d - t_pub_s)/1000000.0; // dt_imu_pub
+
+         if(cnt2==800)
+        {
+             cout<<"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"<<endl;
+             thread_is_running=false;
+             save_file();
+            DXL_Controller_.Torque_OFF_dxls();
+        }
+        cnt2++;       
+    }
+    
+    return;
+}
+
+void init_pose() {
+    Global.BA(X) = C.zmp.previewControl.X.CoM;
+    Global.BA(Y) = C.zmp.previewControl.Y.CoM;
+    Global.BA(Z) = C.cosWave(t, C.zmp.previewControl.PreviewReadyTime, 0.308, 0.25);
+    //coswave를 넣어준 이유 아마 z좌표는 preview control을 해주지 않기 때문에 연속적인 궤적이 나오지 않는다 
+    //따라서 coswave를 통해 궤적을 만들어 준다.
+
+    q.R.pre = KM.Geometric_IK_R(Global.BA, Global.RF);
+    q.L.pre = KM.Geometric_IK_L(Global.BA, Global.LF);
+}
+
+void process() {
+              Global.BA(X) = C.zmp.previewControl.X.CoM;
+              Global.BA(Y) = C.zmp.previewControl.Y.CoM;
+              Global.BA(Z) = 0.25;
+              if (C.walking.step.start_foot == C.walking.step.S_L_F){
+                  Global.BA(Yaw) = C.LFoot.refpos(Yaw);
+              }
+              else if (C.walking.step.start_foot == C.walking.step.S_R_F){
+                  Global.BA(Yaw) = C.RFoot.refpos(Yaw);
+              }
+
+              Global.LF(X) = C.LFoot.refpos(X);
+              Global.LF(Y) = C.LFoot.refpos(Y);
+              Global.LF(Z) = C.LFoot.refpos(Z);
+              Global.LF(Yaw) = C.LFoot.refpos(Yaw);
+
+              Global.RF(X) = C.RFoot.refpos(X);
+              Global.RF(Y) = C.RFoot.refpos(Y);
+              Global.RF(Z) = C.RFoot.refpos(Z);
+              Global.RF(Yaw) = C.RFoot.refpos(Yaw);
+
+              q.L.pre = KM.Geometric_IK_L(Global.BA, Global.LF);
+              q.R.pre = KM.Geometric_IK_R(Global.BA, Global.RF);
+
+              if (C.walking.time.sec >= C.walking.time.periodTime) {//이거 뭐지
+                  C.walking.step.current++;
+                  C.walking.time.sec = 0.0;
+                  C.walking.step.LastLfoot = C.LFoot.refpos;
+                  C.walking.step.LastRfoot = C.RFoot.refpos;
+              }
+    return;
+}
+
+void kick_process() {
+
+    double base_Y = 0.09;
+
+    if (t <= 2*T) {
+
+            Global.BA(X) = 0;
+            Global.BA(Y) = 0;
+            Global.BA(Z) = C.cosWave(t, C.zmp.previewControl.PreviewReadyTime, 0.308, 0.25);
+
+            q.L.pre = KM.Geometric_IK_L(Global.BA, Global.LF);
+            q.R.pre = KM.Geometric_IK_R(Global.BA, Global.RF);
+        }
+
+
+        else if (t > 2*T and 3*T >= t) {
+            Global.BA(X) = 0;
+            Global.BA(Y) = C.cosWave(t-2*T, T, 0, base_Y);
+            Global.BA(Z) = 0.25;
+
+
+            q.L.pre = KM.Geometric_IK_L(Global.BA, Global.LF);
+            q.R.pre = KM.Geometric_IK_R(Global.BA, Global.RF);
+
+
+        }
+
+        else if (t > 3*T and 4*T >= t) {
+            Global.BA(X) = 0;
+            Global.BA(Y) = base_Y;
+            Global.BA(Z) = 0.25;
+
+            Global.LF(X) = 0;
+            Global.LF(Y) = 0.05;
+            Global.LF(Z) = 0;
+            Global.RF(X) = C.cosWave(t-3*T, T, 0, -0.05);
+            Global.RF(Y) = -0.05;
+            Global.RF(Z) = C.cosWave(t-3*T, T, 0, base_Y);
+
+
+            q.L.pre = KM.Geometric_IK_L(Global.BA, Global.LF);
+            q.R.pre = KM.Geometric_IK_R(Global.BA, Global.RF);
+
+
+        }
+
+        else if (t > 4*T and 5*T >= t) {
+            Global.BA(X) = 0;
+            Global.BA(Y) = base_Y;
+            Global.BA(Z) = 0.25;
+
+            Global.LF(X) = 0;
+            Global.LF(Y) = 0.05;
+            Global.LF(Z) = 0;
+            Global.RF(X) = C.cosWave(t-4*T, T, -0.05, 0.1);
+            Global.RF(Y) = -0.05;
+            Global.RF(Z) = C.cosWave(t-4*T, T, base_Y, 0.02);
+
+
+            q.L.pre = KM.Geometric_IK_L(Global.BA, Global.LF);
+            q.R.pre = KM.Geometric_IK_R(Global.BA, Global.RF);
+        }
+
+        else if (t > 5*T and 6*T >= t) {
+            Global.BA(X) = 0;
+            Global.BA(Y) = base_Y;
+            Global.BA(Z) = 0.25;
+
+            Global.LF(X) = 0;
+            Global.LF(Y) = 0.05;
+            Global.LF(Z) = 0;
+            Global.RF(X) = C.cosWave(t-5*T, T, 0.1, 0);
+            Global.RF(Y) = -0.05;
+            Global.RF(Z) = 0.02;
+
+
+            q.L.pre = KM.Geometric_IK_L(Global.BA, Global.LF);
+            q.R.pre = KM.Geometric_IK_R(Global.BA, Global.RF);
+
+
+        }
+
+        else if (t > 6*T and 7*T >= t) {
+            Global.BA(X) = 0;
+            Global.BA(Y) = base_Y;
+            Global.BA(Z) = 0.25;
+
+            Global.LF(X) = 0;
+            Global.LF(Y) = 0.05;
+            Global.LF(Z) = 0;
+            Global.RF(X) = 0;
+            Global.RF(Y) = -0.05;
+            Global.RF(Z) = C.cosWave(t-6*T, T, 0.02, 0);
+
+
+            q.L.pre = KM.Geometric_IK_L(Global.BA, Global.LF);
+            q.R.pre = KM.Geometric_IK_R(Global.BA, Global.RF);
+        }
+
+        else if (t > 7*T and 8*T >= t) {
+            Global.BA(X) = 0;
+            Global.BA(Y) = C.cosWave(t-7*T, T, base_Y, 0);
+            Global.BA(Z) = 0.25;
+
+            Global.LF(X) = 0;
+            Global.LF(Y) = 0.05;
+            Global.LF(Z) = 0;
+            Global.RF(X) = 0;
+            Global.RF(Y) = -0.05;
+            Global.RF(Z) = 0;
+
+
+            q.L.pre = KM.Geometric_IK_L(Global.BA, Global.LF);
+            q.R.pre = KM.Geometric_IK_R(Global.BA, Global.RF);
+
+
+        }
+}
+
+int convertRadTow4095(double rad) {
+    return (int) ((rad + M_PI) * 2048.0 / M_PI);
+}
+
+double convertw4095ToRad(int w4095) {
+    return (w4095 - 2048) * M_PI / 2048.0;
+}
+
+void catch_signal(int sig) {
+    //signal(sig, SIG_IGN);
+    printf("Program END...\n");
+    cout<<"program end"<<endl;
+
+    //close(dxl.serial_port);
+    printf("Program END...\n");
+    ros::shutdown();
+    exit(0);
+}
